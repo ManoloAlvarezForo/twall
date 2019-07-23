4bac18ad27c0cb94b1af4dd0849a17b8bcead5cd
Manolo Alvarez <manolo.alvarez@avantica.net>
2019-07-23 18:24:33 -0400

    fix-production-issue: A fix for the moment was added for the lazy import in React

--- app/main.dev.js
+++ app/main.dev.js
@@ -40,7 +40,7 @@ if (
 const installExtensions = async () => {
   const installer = require('electron-devtools-installer');
   const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-  const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS'];
+  const extensions = ['REACT_DEVELOPER_TOOLS'];
 
   return Promise.all(
     extensions.map(name => installer.default(installer[name], forceDownload))
@@ -68,6 +68,9 @@ app.on('ready', async () => {
   }
 
   mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    },
     show: false,
     width: 1024,
     height: 728
